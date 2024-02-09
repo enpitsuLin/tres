6f38a1f135d571206659231641bb5586aa2818ee
enpitsulin <enpitsulin@gmail.com>
2024-02-09 10:55:48 +0800

    chore: copy all unnecessary file to zh dir

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitepress'
 import { enConfig } from './en'
 import { esConfig } from './es'
 import { sharedConfig } from './shared'
-
-/* import { zhConfig } from './zh' */
+import { zhConfig } from './zh' 
 
 export default defineConfig({
   ...sharedConfig,
@@ -11,6 +10,6 @@ export default defineConfig({
   locales: {
     root: { label: 'English', lang: 'en-US', link: '/', ...enConfig },
     es: { label: 'Español', lang: 'es-ES', link: '/es/', ...esConfig },
-    /* zh: { label: '简体中文', lang: 'zh-CN', link: '/zh/', ...zhConfig }, */
+    zh: { label: '简体中文', lang: 'zh-CN', link: '/zh/', ...zhConfig }, 
   },
 })
